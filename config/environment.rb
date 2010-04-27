96839d9d728fee37a8ec814b637336447032ebad
foi <foi.first@gmail.com>
2010-04-27 19:51:07 +0800

    many minors

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,12 @@ ENV['RECAPTCHA_PRIVATE_KEY'] = '6Lc9fwkAAAAAAGILDTn9uqw4brwDBzEpkuSwPMGH'
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-	#config.gem "racc", :source => "http://gems.rubyforge.org"
-	config.gem "rexical", :source => "http://rubygems.org"
-	#config.gem "rake-compiler", :source => "http://rubygems.org"
 	config.gem "acts-as-taggable-on", :source => "http://rubygems.org"
-	config.gem "nokogiri", :source => "http://gems.rubyforge.org"
+	config.gem "nokogiri"
 	config.gem "RedCloth", :lib => 'redcloth'
-	config.gem 'will_paginate', :source => 'http://gems.github.com'
+	config.gem 'will_paginate'
 	config.action_controller.session_store = :active_record_store
 	config.time_zone = 'UTC'
-	
 end
 
 
