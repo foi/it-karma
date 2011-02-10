78d85ed494a9cb41d44a19777db528864cac0e35
unknown <foi.first@gmail.com>
2011-02-10 11:33:33 +0700

    about

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -10,10 +10,10 @@ class SessionController < ApplicationController
 	
 	def authenticate
 		authenticate_or_request_with_http_basic do |n, p|
-			n == "1" && p == "2" 
+			n == "foi" && p == "1" 
 			@n, @p = n, p
 		end
-		if @n == "1" && @p == "2" 
+		if @n == "foi" && @p == "1" 
 			session[:admin] = true
 			redirect_to root_url
 		end
